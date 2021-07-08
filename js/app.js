490832d7048a425fcb817eb6d97d31e7f40707ca
Alexx-Viv <mario.alex.vivsenco@gmail.com>
2021-07-08 17:33:40 +0200

    Almost done

--- js/app.js
+++ js/app.js
@@ -2,32 +2,49 @@ var nameField = document.getElementById("name");
 var kmField = document.getElementById("km");
 var ageField = document.getElementById("age");
 var button = document.getElementById("generate-ticket");
+var reset = document.getElementById("reset");
 
 var passengerName = document.getElementById("passenger-name");
 var discount = document.getElementById("discount");
 var cab = document.getElementById("cab");
-var trainCode = document.getElementById("traine-ticket");
+var trainCode = document.getElementById("train-code");
 var ticketPrice = document.getElementById("ticket-price");
 
 button.addEventListener("click", function () {
   var nameValue = nameField.value;
   var kmValue = kmField.value;
   var ageValue = ageField.value;
-
-  console.log(nameValue);
-  console.log(kmValue);
-  console.log(ageValue);
-
+  var trainCodeValue = trainCode;
   var price = 0.21 * kmValue;
   var discountValue = "Tariffa ordinaria";
 
-  if(ageValue === "min"){
-      price *= 0.8;
-      discountValue = "Sconto per Junior applicato";
-  } else if ( ageValue === "max"){
-      price *= 0.4;
-      discountValue = "Sconto per Senior applicato";
+  if (ageValue === "min") {
+    price *= 0.8;
+    discountValue = "Sconto per Junior applicato";
+  } else if (ageValue === "max") {
+    price *= 0.4;
+    discountValue = "Sconto per Senior applicato";
   }
 
-  console.log(price);
+  price = "Euro " + price.toFixed(2);
+
+  var cabNumber = Math.floor(Math.random() * 12) + 1;
+
+  passengerName.innerHTML = " " + nameValue;
+  discount.innerHTML = " " + discountValue;
+  cab.innerHTML = " " + cabNumber;
+  ticketPrice.innerHTML = " " + price;
+  trainCodeValue.innerHTML = "74837";
+});
+
+reset.addEventListener("click", function () {
+  nameField.value = "";
+  kmField.value = "10";
+  ageField.value = "mag";
+
+  passengerName.innerHTML = "";
+  discount.innerHTML = "";
+  cab.innerHTML = "";
+  ticketPrice.innerHTML = "";
+  trainCode.innerHTML = "";
 });
